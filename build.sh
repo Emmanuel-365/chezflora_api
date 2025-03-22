7c2e7020dfbaaff80de1724d0c69c9c90ac3b603
Emmanuel <emmanuelscre1@gmail.com>
2025-03-22 09:09:23 +0100

    test before revert

--- build.sh
+++ build.sh
@@ -2,5 +2,4 @@
 pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py migrate
-python manage.py loaddata db_dump.json  # Charge les données
 python manage.py collectstatic --noinput
