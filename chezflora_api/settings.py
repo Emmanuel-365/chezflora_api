ea63f72234b26f6933e193f99c3333300a726d5f
Emmanuel <emmanuelscre1@gmail.com>
2025-03-22 12:34:10 +0100

    autorisation de toutes les adresses via CORS

--- chezflora_api/settings.py
+++ chezflora_api/settings.py
@@ -118,6 +118,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
 # CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS', default='http://localhost:3000').split(',')
 
 CORS_ALLOW_CREDENTIALS = True  # Pour les cookies/auth
