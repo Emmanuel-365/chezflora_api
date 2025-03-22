cff98b9edd035bcdc9b7c919250fd80325aa828f
Emmanuel <emmanuelscre1@gmail.com>
2025-03-22 09:04:49 +0100

    Migration vers SQLite en production avec données MySQL

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py migrate
+python manage.py loaddata db_dump.json  # Charge les données
 python manage.py collectstatic --noinput
