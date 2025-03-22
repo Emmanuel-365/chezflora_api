7c1c57dd895a5cd2eeb9483d51f42cfdc7c9e6e2
Emmanuel <emmanuelscre1@gmail.com>
2025-03-22 11:49:09 +0100

    dump mis en commentaire

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py migrate
-python manage.py loaddata db_dump.json
+# python manage.py loaddata db_dump.json
 python manage.py collectstatic --noinput
