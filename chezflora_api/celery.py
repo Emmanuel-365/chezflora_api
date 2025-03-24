c689e10230c7cc19be52dbc5356ff2db1cd7ef5c
Emmanuel <emmanuelscre1@gmail.com>
2025-03-24 09:57:25 +0100

    update celery.py

--- chezflora_api/celery.py
+++ chezflora_api/celery.py
@@ -1,7 +1,18 @@
+# chezflora_api/celery.py
 import os
 from celery import Celery
 
+# Définir les paramètres Django pour Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chezflora_api.settings')
-app = Celery('chezflora_api')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
+
+celery_app = Celery('chezflora_api')
+
+# Charger les paramètres à partir des settings Django
+celery_app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Auto-détection des tâches dans toutes les apps installées
+celery_app.autodiscover_tasks()
+
+@celery_app.task(bind=True)
+def debug_task(self):
+    print(f'Requête : {self.request!r}')
